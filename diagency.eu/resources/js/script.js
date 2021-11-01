7e46b0f904e55bf0621ef76a2b387ff87f7e813e
Z0rg3D <bucel.ionsebastian@gmail.com>
2021-11-01 19:17:15 +0200

    added diagency website

---
+++ diagency.eu/resources/js/script.js
@@ -0,0 +1,28 @@
+
+
+function toggleMenu() {
+    document.getElementById("menu").classList.toggle("inactive-animation");
+    document.getElementById("menu").classList.toggle("active-animation");
+    document.getElementById("menu").classList.toggle("change");
+    document.getElementById("main-menu").classList.toggle("change-main-menu");
+    document.getElementById("main-menu-border-1").classList.toggle("activeBorder");
+    setTimeout(() => {
+        document.getElementById("main-menu-border-2").classList.toggle("activeBorder");
+        document.getElementById("main-menu-li-a-1").classList.toggle("activeLink");
+        setTimeout(() => {
+            document.getElementById("main-menu-border-3").classList.toggle("activeBorder");
+            document.getElementById("main-menu-li-a-2").classList.toggle("activeLink");
+            setTimeout(() => {
+                document.getElementById("main-menu-border-4").classList.toggle("activeBorder");
+                document.getElementById("main-menu-li-a-3").classList.toggle("activeLink");
+                setTimeout(() => {
+                    document.getElementById("main-menu-li-a-4").classList.toggle("activeLink");
+                    setTimeout(() => {
+                        document.getElementById("main-menu-li-a-5").classList.toggle("activeLink");
+
+                    }, 100);
+                }, 100);
+            }, 100);
+        }, 100);
+    }, 100);
+}
