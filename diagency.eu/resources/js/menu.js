212c3db7128df341ccaae49e9015437a9b847d05
Bucel Sebastian <bucel.ionsebastian@gmail.com>
2022-08-08 15:24:24 +0300

    End of the day 08/08/2022

--- diagency.eu/resources/js/menu.js
+++ diagency.eu/resources/js/menu.js
@@ -1,16 +1,22 @@
 let menuBtns = document.querySelectorAll(".burger-menu");
 
+let menuAnim = 
+
 menuBtns.forEach(menuBtn => {
     menuBtn.addEventListener("click",e=>{
         if(menuBtn.classList.contains("menu-open")){
 
             menuBtns.forEach(elem =>{
                 elem.classList.remove("menu-open");
-            })
+                elem.classList.add("closed-menu");
+            });
+
+
         }
         else{
 
             menuBtns.forEach(elem =>{
+                elem.classList.remove("closed-menu");
                 elem.classList.add("menu-open");
             })
         }
